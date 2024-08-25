263dcbdbc880f81ee145e5d3fbeb1a40b75f9784
陳宣廷 <113078480+xuanting09@users.noreply.github.com>
2024-08-26 00:19:06 +0900

    text bingo ok

--- Assets/DraggableItem.cs
+++ Assets/DraggableItem.cs
@@ -1,40 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;  // 添加这个命名空间
 
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public Image image;
     [HideInInspector] public Transform parentAfterDrag;
 
-    // 添加對 InventorySlot (25) 的引用
+    // 初始槽位的引用
     public Transform initialSlot;
 
     void Start()
     {
-        // 將初始父物件設為 InventorySlot (25)
-        transform.SetParent(initialSlot);
-        transform.localPosition = Vector3.zero; // 重置位置相對於父物件
+        // 将初始父物体设置为 InventorySlot
+        if (initialSlot != null)
+        {
+            transform.SetParent(initialSlot);
+            transform.localPosition = Vector3.zero; // 重置位置相对于父物体
+        }
     }
 
-    public void OnBeginDrag(PointerEventData eventData) {
+    public void OnBeginDrag(PointerEventData eventData)
+    {
         Debug.Log("Begin drag");
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
-        image.raycastTarget = false;
+
+        // 禁用 Text (TMP) 的 Raycast Target
+        GetComponent<TextMeshProUGUI>().raycastTarget = false;
+        Debug.Log("Text Raycast target disabled");
     }
 
-    public void OnDrag(PointerEventData eventData) {
+    public void OnDrag(PointerEventData eventData)
+    {
         Debug.Log("Dragging");
         transform.position = Input.mousePosition;
     }
 
-    public void OnEndDrag(PointerEventData eventData) {
+    public void OnEndDrag(PointerEventData eventData)
+    {
         Debug.Log("End drag");
-        transform.SetParent(parentAfterDrag);
-        image.raycastTarget = true;
+
+        // 获取鼠标位置下的 UI 对象
+        GameObject dropTarget = eventData.pointerCurrentRaycast.gameObject;
+
+        if (dropTarget != null)
+        {
+            Debug.Log("Raycast hit: " + dropTarget.name);
+
+            // 检查是否放置在 InventorySlot 上
+            InventorySlot slot = dropTarget.GetComponent<InventorySlot>();
+            if (slot != null)
+            {
+                transform.SetParent(dropTarget.transform);
+                transform.localPosition = Vector3.zero;
+                Debug.Log("Dropped into: " + dropTarget.name);
+            }
+            else
+            {
+                // 如果不是放在有效的 InventorySlot 上，则返回原始位置
+                transform.SetParent(parentAfterDrag);
+                transform.localPosition = Vector3.zero;
+                Debug.Log("Dropped back to original parent: " + parentAfterDrag.name);
+            }
+        }
+        else
+        {
+            // 如果 Raycast 没有命中任何对象，则返回原始位置
+            transform.SetParent(parentAfterDrag);
+            transform.localPosition = Vector3.zero;
+            Debug.Log("Raycast missed. Dropped back to original parent: " + parentAfterDrag.name);
+        }
+
+        // 恢复 Text (TMP) 的 Raycast Target
+        GetComponent<TextMeshProUGUI>().raycastTarget = true;
+        Debug.Log("Text Raycast target enabled");
     }
 }
--- Assets/InventorySlot.cs
+++ Assets/InventorySlot.cs
@@ -6,11 +6,20 @@ using UnityEngine.EventSystems;
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData) {
-        if (transform.childCount == 0) {
-            GameObject dropped = eventData.pointerDrag;
+        // 检查是否有物体被拖拽
+        GameObject dropped = eventData.pointerDrag;
+        if (dropped != null)
+        {
             DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
-            draggableItem.parentAfterDrag = transform;
+            // 检查拖拽物体是否有 DraggableItem 组件
+            if (draggableItem != null)
+            {
+                // 如果 InventorySlot 没有子物体，则将拖拽物体放入
+                if (transform.childCount == 0) 
+                {
+                    draggableItem.parentAfterDrag = transform;
+                }
+            }
         }
     }
-
 }
